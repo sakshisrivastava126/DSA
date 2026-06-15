904482e82c984e75fc5bbc3849a77df0d6effe74
Sakshi Srivastava <srivastavasakshi126@gmail.com>
2026-06-15 12:37:00 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (72.53%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    bool search(vector<int>& arr, int tar) {
-        int l = 0, h = arr.size()-1;
+    bool search(vector<int>& nums, int tar) {
+        int l=0, h= nums.size()-1;
 
-        while(l <= h && h<arr.size()){
-            int mid = l + (h-l)/2;
-            if(arr[mid] == tar) return true;
-            if(arr[l] == arr[mid] && arr[h] == arr[mid]){
+        while(l <=h ){
+            int mid = (h+l)/2;
+            if(tar == nums[mid]) return true;
+
+            if(nums[mid] == nums[l] && nums[mid]==nums[h]){
                 l++, h--;
                 continue;
             }
 
-            if(arr[l] <= arr[mid]){
-                if(arr[l] <= tar && tar <= arr[mid]) h = mid-1;
-                else l = mid+1;
+            if(nums[l] <= nums[mid]){
+                if(nums[l] <= tar && tar <= nums[mid]) h = mid-1;
+                else l = mid+1; 
             }
             else{
-                if(arr[mid] <= tar && tar <= arr[h]) l = mid+1;
+                if(nums[mid] <= tar && tar <= nums[h]) l=mid+1;
                 else h = mid-1;
             }
         }
