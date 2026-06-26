1a4875cce14f7528067660c29ddfa2e99959f68a
Sakshi Srivastava <srivastavasakshi126@gmail.com>
2026-06-26 09:42:19 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (10.34%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    bool check(vector<int>& arr, int k, int mid){
-        int i=0; int cnt=1; int curr=0;
-
-        for(int i = 0; i < arr.size(); i++){
-            if(curr + arr[i] <= mid){
-                curr += arr[i];
-            } 
-            else {
-                cnt++;
-                curr = arr[i];
+    bool check(vector<int>& nums, int mid, int k){
+        int parts=0;
+        int curr=0;
+        for(auto it : nums){
+            if(curr + it <= mid){
+                curr += it;
+            }
+            else{
+                parts++;
+                curr = it;
             }
         }
-        return cnt<=k;
+        return parts+1 <=k;
     }
     int splitArray(vector<int>& nums, int k) {
         int n = nums.size();
-        int total = 0;
-        for(auto it : nums) total += it;
 
-        int l = *max_element(nums.begin(), nums.end());
-        int h = total;
+        int sum=0;
+        for(auto it : nums) sum += it;
+
+        int l= *max_element(nums.begin(), nums.end()), h=sum;
+        int ans=0;
         
-        int ans = 0;
-
         while(l <= h){
             int mid = l + (h-l)/2;
 
-            if(check(nums, k, mid)){
+            if(check(nums, mid, k)){
                 ans = mid;
                 h = mid-1;
             }
