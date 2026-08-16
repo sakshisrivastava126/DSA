18acb809e5f43e95bf0f637ce7522f200d233014
Sakshi Srivastava <srivastavasakshi126@gmail.com>
2026-08-16 12:54:22 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (83.24%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i=0, j=0;
+        for(i=0; i<nums.size(); i++){
+            if(nums[i]!=0){
+                swap(nums[i], nums[j]);
+                j +=1;
+            }
+        }
+    }
+};
