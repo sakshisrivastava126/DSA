c6a1c62697deaff03846cf90e1bcd4f64c57ae26
Sakshi Srivastava <srivastavasakshi126@gmail.com>
2026-09-12 15:09:15 +0530

    Time: 0 ms (100.00%) | Memory: 97.3 MB (88.08%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,9 +4,9 @@ public:
         int mini=prices[0];
         int ans=0;
         for(int i=1; i<prices.size(); i++){
-            
-            ans = max(ans, prices[i]-mini);
             mini = min(mini, prices[i]);
+            ans = max(ans, prices[i]-mini);
+            
         }
         return ans;
     }
